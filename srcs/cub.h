58f8166a0a532916f3ab6e6adb36f39374b0d64f
user42 <coureau.paco@gmail.com>
2020-10-11 19:08:08 +0200

    so, previous commit wasnt cool, it was not utils but walls.c. Then norm is almost ok, cub.h is weird...

--- srcs/cub.h
+++ srcs/cub.h
@@ -10,9 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** libraries **
-*/
 #include "../minilibx-linux/mlx.h"
 #include "../libft/libft.h"
 
@@ -49,9 +46,8 @@
 #define KEY_LEFT 65361
 #define KEY_RIGHT 65363
 
-
 /*
-**	keys pre-setted values mapping LibX Functions 
+** keys pre-setted values mapping LibX Functions
 */
 #define KEY_PRESS 2
 #define KEY_RELEASE 3
@@ -66,16 +62,19 @@
 #define RED 0x00FF0000
 #define BLUE 0x000000FF
 #define YELLOW 0x00FFFF00
-//#define PINK 0x00FFC0CB
 #define PINK 9961608
 
-/* windows */
+/*
+** windows
+*/
 #define WIDTH 1280
 #define HEIGHT 680
 #define WIN_NAME "Cub3D"
 #define BPP 4
 
-/* moves settings */
+/*
+** moves settings
+*/
 #define SPEED 0.2
 #define ANGLE 0.1
 #define FOV 0.7
@@ -99,7 +98,7 @@ typedef struct		s_win
 	int				y;
 }					t_win;
 
-typedef struct	s_img
+typedef struct		s_img
 {
 	void			*ptr;
 	unsigned int	*adr;
@@ -113,6 +112,12 @@ typedef struct		s_color
 	unsigned int	*color;
 }					t_color;
 
+/*
+** pos = where to start drawing texture
+** step = ratio to increase or decrease texture bloc size
+** x, y = coordinate in the texture
+** width = texture bloc width
+*/
 typedef struct		s_tex
 {
 	unsigned int	*n;
@@ -120,62 +125,94 @@ typedef struct		s_tex
 	unsigned int	*w;
 	unsigned int	*e;
 	unsigned int	*sprite;
-	double			pos; /* where to start drawing texture */
-	double			step; /* ratio to increase or decrease texture bloc size */
-	int				x; /* x coordinate in the texture */
-	int				y; /* y coordinate in the texture */
-	int				width; /* texture bloc width */
+	double			pos;
+	double			step;
+	int				x;
+	int				y;
+	int				width;
 }					t_tex;
 
-typedef struct	s_map
+typedef struct		s_map
 {
 	char			**tab;
 	int				h;
 	int				x;
 	int				y;
 	int				sprite_nb;
-	
+
 }					t_map;
 
+/*
+** pos = ray position
+** dir = ray direction
+** sd = side_dist, dist from player to the next map square until hitting a wall
+** dd = delta_dist, dist from the ray.pos to the next map square
+*/
 typedef struct		s_ray
 {
-	t_int_xy		pos; /* ray position */
-	t_double_xy		dir; /*ray direction*/ 
-	t_double_xy		sd; /* side_dist : dist from player to the next map square until hitting a wall */
-	t_double_xy		dd; /* delta_dist : dist from the ray_pos to the next map square */
+	t_int_xy		pos;
+	t_double_xy		dir;
+	t_double_xy		sd;
+	t_double_xy		dd;
 }					t_ray;
 
-typedef struct	s_sprite
+/*
+** start = when to start drawing the sprite on the y coordinate
+** end = when to stop drawing the sprite on the y coordinate
+** delta = relative x and y coordinate from the player
+*/
+typedef struct		s_sprite
 {
 	int				height;
 	int				width;
 	int				screen;
 	double			dist;
-	t_int_xy		start; /* y coordinate to start drawing the wall */
-	t_int_xy		end; /* y coordinate to end drawing the wall */
+	t_int_xy		start;
+	t_int_xy		end;
 	t_double_xy		pos;
-	t_double_xy		delta; /* relative x and y coordinate from the player */
+	t_double_xy		delta;
 	t_double_xy		depth;
 }					t_sprite;
 
+/*
+** step = variable to iterate every square of the map, step is negative
+**	if ray_dir < 0, so also tells us if the wall hit
+**	is NSEW combined with the side value
+** side = which side of the wall is hitten (x or y coordinate)
+**	to know what texture to select
+** hit = if 1, the wall is hit, so quit the loop
+** height = height of the wall, calculated from the distance
+** start = when to start drawing the wall on the y coordinate
+** end = when to stop drawing the wall on the y coordinate
+** perp_dist = perpendicular distance of the wall from the player plane
+** x = x coordinate of the hitten wall, in order to know the x coordinate
+**	of the texture to pick
+** buf = buffer to keep wall distances in order to sorte sprite and
+**	not display sprites behind walls (very important)
+*/
 typedef	struct		s_wall
 {
-	t_int_xy		step; /* variable to iterate every square of the map, step is negative if ray_dir < 0, so also tells us if the wall hit is NSEW combined with the side value */
-	int				side; /* wich side of the wall is hitten (x or y coordinate) to know to texture to select */
-	int				hit; /* hit = 1 if the wall is hitten, in order to quite the loop */
-	int				height; /* height of the wall calculated from the distance */
-	int				start; /* y coordinate where to start drawing the wall stripe */
-	int				end; /* y coordinate where to start drawing the wall stripe */
-	double			perp_dist; /* perpendicular distance of wall from the player plane */
-	double			x; /*x coordinate of the hitten wall, in order to know the x coordinate of the texture to pick */
-	double			*buf; /* buffer to keep wall distances in order to sorte sprite and not display sprites behind walls */
+	t_int_xy		step;
+	int				side;
+	int				hit;
+	int				height;
+	int				start;
+	int				end;
+	double			perp_dist;
+	double			x;
+	double			*buf;
 }					t_wall;
 
+/*
+** pos = player position
+** dir = direction of the player
+** plane = camera plane, wich is always perpendicular to player dir
+*/
 typedef struct		s_player
 {
-	t_double_xy		pos;	// player position on the map
-	t_double_xy		dir;	// direction of the player (vector)
-	t_double_xy		plane;	/* camera plane, which is always perpendicular to player dir */
+	t_double_xy		pos;
+	t_double_xy		dir;
+	t_double_xy		plane;
 }					t_player;
 
 typedef struct		s_parsing
@@ -185,9 +222,14 @@ typedef struct		s_parsing
 	int				color;
 }					t_parsing;
 
+/*
+** cam = x cam coordinate, -1 = left, 0 = center, 1 = right
+** color = the color of the pixel
+** cub = map file name
+*/
 typedef struct		s_struct
 {
-	void			*mlx;	//mlx pointer
+	void			*mlx;
 	t_parsing		parse;
 	t_win			win;
 	t_img			img;
@@ -199,87 +241,110 @@ typedef struct		s_struct
 	t_ray			ray;
 	t_wall			wall;
 	t_sprite		*sprite;
-	double			cam;	/* x cam coordinate : -1 = left | 0 = center | 1 = right */
-	unsigned int	color;	/* pixel color to draw */
-	char			*cub;	//map file name
-	int				i;		//counter
-	int				x;		//x coordinate of the screen 0 is top left
-	int				y;		//y coordinate of the screen 0 is top left
+	double			cam;
+	unsigned int	color;
+	char			*cub;
+	int				i;
+	int				x;
+	int				y;
 }					t_struct;
 
-
-/* main.c */
+/*
+** main.c
+*/
 void				ft_init(char *av1, int arg);
 void				ft_init_ii(t_struct *s);
 void				ft_init_iii(t_struct *s);
 void				ft_init_raycasting_data(t_struct *s);
 
-/* parse.c */
+/*
+** parse.c
+*/
 void				ft_parse(t_struct	*s);
 void				ft_read_line(t_struct *s, char	*tmp);
 void				ft_resolution(t_struct *s, char	*tmp);
 t_color				ft_color(t_struct *s, char	*tmp);
 unsigned int		*ft_load_tex(t_struct *s, char *tmp);
 
-/* parse_map.c */
+/*
+** parse_map.c
+*/
 void				ft_get_pos(t_struct *s);
 void				ft_load_map(t_struct *s, char *line);
 void				ft_check_map_char(t_struct *s, int x, int y);
 int					ft_check_map_borders(t_struct *s, int x, int y, char **map);
 void				ft_check_map(t_struct *s);
 
-/* utils.c */
+/*
+** utils.c
+*/
 int					ft_is_space(char c);
 void				ft_skip_space(t_struct *s, char *line);
 int					ft_is_charset(char c, char *set);
 int					ft_suffix(char *file_name, char *suffix);
 char				**ft_new_tab(t_struct *s, char **tab, char *str);
 
-/* utilsii.c */
+/*
+** utilsii.c
+*/
 int					ft_exit(t_struct *s);
 void				ft_error(t_struct *s, int err);
 int					ft_check_parsing(t_struct *s);
 void				ft_resolutionii(t_struct *s, int n);
 
-/* print.c */
+/*
+** print.c
+*/
 void				ft_print_arg(t_struct *s);
 void				print_map(t_struct *s);
 void				print_sprite(t_struct *s);
 
-/* walls.c */
+/*
+** walls.c
+*/
 void				ft_wall(t_struct *s);
 void				ft_ray_init(t_struct *s);
 void				ft_calculsd(t_struct *s);
 void				ft_hitwall(t_struct *s);
-void			    ft_wallsize(t_struct *s);
+void				ft_wallsize(t_struct *s);
 
-/* wallsii.c */
-void    			ft_walltex(t_struct *s);
-void    			ft_drawwall(t_struct *s);
-void    			ft_pixel(t_struct *s);
+/*
+** wallsii.c
+*/
+void				ft_walltex(t_struct *s);
+void				ft_drawwall(t_struct *s);
+void				ft_pixel(t_struct *s);
 
-/* move.c */
+/*
+** move.c
+*/
 int					key_press(int key, t_struct *s);
 void				ft_move_forward(t_struct *s, double sign);
 void				ft_move_side(t_struct *s, double sign);
 void				ft_rotate(t_struct *s, double sign);
 void				ft_event(t_struct *s);
 
-/* sprite.c */
+/*
+** sprite.c
+*/
 void				ft_sprite(t_struct *s);
 void				ft_countsprite(t_struct *s);
 void				ft_spritepos(t_struct *s);
 void				ft_spritedis(t_struct *s);
 void				ft_spritetri(t_struct *s);
 
-/* spriteii.c */
+/*
+** spriteii.c
+*/
 void				ft_spritetrans(t_struct *s);
 void				ft_spritesize(t_struct *s);
 void				ft_drawsprite(t_struct *s);
 
-/* bitmap.c */
-void			ft_bitmap(t_struct *s);
-unsigned char	*ft_bmp_file_header(t_struct *s);
-unsigned char	*ft_bmp_dib_header(t_struct *s);
-unsigned char	*ft_bmp_pixel_array(t_struct *s);
-void			ft_set_int_to_char(unsigned char *start, int value);
+/*
+** bitmap.c
+*/
+void				ft_bitmap(t_struct *s);
+unsigned char		*ft_bmp_file_header(t_struct *s);
+unsigned char		*ft_bmp_dib_header(t_struct *s);
+unsigned char		*ft_bmp_pixel_array(t_struct *s);
+void				ft_set_int_to_char(unsigned char *start, int value);
--- srcs/wallsii.c
+++ srcs/wallsii.c
@@ -12,55 +12,53 @@
 
 #include "cub.h"
 
-void    ft_pixel(t_struct *s)
+void	ft_pixel(t_struct *s)
 {
-    int p;
+	int p;
 
-    p = s->tex.width * s->tex.y + s->tex.x;
-    if (s->wall.side == 0)
-    {
-        if (s->wall.step.x < 0)
-            s->img.adr[s->x + s->win.x * s->y] = s->tex.w[p];
-        else
-            s->img.adr[s->x + s->win.x * s->y] = s->tex.e[p];
-    }
-    else
-    {
-        if (s->wall.step.y < 0)
-            s->img.adr[s->x + s->win.x * s->y] = s->tex.n[p];
-        else
-            s->img.adr[s->x + s->win.x * s->y] = s->tex.s[p];
-    }
+	p = s->tex.width * s->tex.y + s->tex.x;
+	if (s->wall.side == 0)
+	{
+		if (s->wall.step.x < 0)
+			s->img.adr[s->x + s->win.x * s->y] = s->tex.w[p];
+		else
+			s->img.adr[s->x + s->win.x * s->y] = s->tex.e[p];
+	}
+	else
+	{
+		if (s->wall.step.y < 0)
+			s->img.adr[s->x + s->win.x * s->y] = s->tex.n[p];
+		else
+			s->img.adr[s->x + s->win.x * s->y] = s->tex.s[p];
+	}
 }
 
-void    ft_drawwall(t_struct *s)
+void	ft_drawwall(t_struct *s)
 {
-    s->color = s->sky.r * pow(256, 2) + s->sky.g * 256 + s->sky.b;
-    s->y = 0;
-    while (s->y < s->wall.start)
-        s->img.adr[s->x + s->win.x * s->y++] = s->color;
-    while (s->y >= s->wall.start && s->y <= s->wall.end)
-    {
-        s->tex.y = (int)s->tex.pos;
-        s->tex.pos += s->tex.step;
-        ft_pixel(s);
-        s->y++;
-    }
-    s->color = s->floor.r * pow(256, 2) + s->floor.g * 256 + s->floor.b;
-    while (s->y < s->win.y)
-	    s->img.adr[s->x + s->win.x * s->y++] = s->color;
+	s->color = s->sky.r * pow(256, 2) + s->sky.g * 256 + s->sky.b;
+	s->y = 0;
+	while (s->y < s->wall.start)
+		s->img.adr[s->x + s->win.x * s->y++] = s->color;
+	while (s->y >= s->wall.start && s->y <= s->wall.end)
+	{
+		s->tex.y = (int)s->tex.pos;
+		s->tex.pos += s->tex.step;
+		ft_pixel(s);
+		s->y++;
+	}
+	s->color = s->floor.r * pow(256, 2) + s->floor.g * 256 + s->floor.b;
+	while (s->y < s->win.y)
+		s->img.adr[s->x + s->win.x * s->y++] = s->color;
 }
 
-void    ft_walltex(t_struct *s)
+void	ft_walltex(t_struct *s)
 {
-    s->tex.x = (int)(s->wall.x * s->tex.width);
-//    if ((s->wall.side == 0 && s->ray.dir.x > 0) ||
-//        (s->wall.side == 1 && s->ray.dir.y < 0))
-//        s->tex.x = s->tex.width - s->tex.x - 1;
-    if (s->wall.side == 0 && s->ray.dir.x > 0)
-	    s->tex.x = s->tex.width - s->tex.x-1;
-    if (s->wall.side == 1 && s->ray.dir.y < 0)
-	    s->tex.x = s->tex.width - s->tex.x -1;
-    s->tex.step = (double)s->tex.width / s->wall.height;
-    s->tex.pos = (s->wall.start - s->win.y / 2 + s->wall.height / 2) * s->tex.step;
+	s->tex.x = (int)(s->wall.x * s->tex.width);
+	if (s->wall.side == 0 && s->ray.dir.x > 0)
+		s->tex.x = s->tex.width - s->tex.x - 1;
+	if (s->wall.side == 1 && s->ray.dir.y < 0)
+		s->tex.x = s->tex.width - s->tex.x - 1;
+	s->tex.step = (double)s->tex.width / s->wall.height;
+	s->tex.pos = (s->wall.start - s->win.y / 2 + s->wall.height / 2)
+		* s->tex.step;
 }
